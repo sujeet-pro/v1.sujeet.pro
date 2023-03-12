f8ffafe524d3a6f73880182b8e0a264dbaab18fb
Sujeet Jaiswal <contact@sujeet.pro>
2023-03-12 12:31:45 +0530

    feat(Seo and analytics)

--- src/constants/global.ts
+++ src/constants/global.ts
@@ -1,1 +1,2 @@
-export const SITE_BASE = 'https://sujeet.pro'
+export const SITE_BASE = "https://sujeet.pro";
+export const AUTHOR_NAME = "Sujeet Jaiswal";
--- src/content/config.ts
+++ src/content/config.ts
@@ -4,16 +4,16 @@ const seoConfig = {
   description: z.string(),
   isDraft: z.boolean().optional(),
   name: z.string().optional(),
-  tags: z.string().array(),
-  coverImg: z.string().optional()
-}
+  tags: z.string().array().optional(),
+  coverImg: z.string().optional(),
+};
 
 const blogCollection = defineCollection({
   schema: z.object({
     ...seoConfig,
     publishedOn: z.date(),
     lastUpdatedOn: z.date().optional(),
-    topics: z.string().array()
+    topics: z.string().array(),
   }),
 });
 
@@ -28,12 +28,12 @@ const projectCollection = defineCollection({
 const topicCollection = defineCollection({
   schema: z.object({
     ...seoConfig,
-    iconName: z.string().optional()
-  })
-})
+    iconName: z.string().optional(),
+  }),
+});
 
 export const collections = {
   blog: blogCollection,
   project: projectCollection,
-  topic: topicCollection
+  topic: topicCollection,
 };
