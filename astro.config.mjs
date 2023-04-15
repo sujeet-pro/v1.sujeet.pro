357ecd8a9fa233efe955e9be871de1815c575d46
Sujeet Jaiswal <contact@sujeet.pro>
2023-04-15 11:55:12 +0530

    feat: updated profile

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,36 +1,31 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+import mdx from "@astrojs/mdx";
 import remarkToc from "remark-toc";
 // import { rehypeHeadingIds } from "@astrojs/markdown-remark";
 import { rehypeAccessibleEmojis } from "rehype-accessible-emojis";
 import rehypeSlug from "rehype-slug";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 
-// https://astro.build/config
-import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://sujeet.pro",
   experimental: {
-    assets: true,
+    assets: true
   },
   markdown: {
-    remarkPlugins: [
-      [
-        remarkToc,
-        {
-          heading: "Table of Contents",
-          maxDepth: 2,
-        },
-      ],
-    ], // { heading: "contents" } to change where to inject
-    rehypePlugins: [
-      rehypeSlug,
-      // rehypeHeadingIds,
-      [rehypeAutolinkHeadings, { behavior: "append" }],
-      rehypeAccessibleEmojis,
-    ],
+    remarkPlugins: [[remarkToc, {
+      heading: "Table of Contents",
+      maxDepth: 2
+    }]],
+    // { heading: "contents" } to change where to inject
+    rehypePlugins: [rehypeSlug,
+    // rehypeHeadingIds,
+    [rehypeAutolinkHeadings, {
+      behavior: "append"
+    }], rehypeAccessibleEmojis]
   },
-  integrations: [tailwind(), sitemap()],
+  integrations: [tailwind(), sitemap(), mdx()]
 });
